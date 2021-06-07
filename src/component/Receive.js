222851d7654ef9835af3f7b531b5acca42c6a65b
eehd80 <eehd80yul@gmail.com>
2021-06-07 12:06:36 +0900

    datepicker

--- src/component/Receive.js
+++ src/component/Receive.js
@@ -14,9 +14,12 @@ const disabilityType = [
     { value: "vanilla", label: "Vanilla" },
 ];
 const disabilityGrade = [
+    { value: "1급", label: "1급" },
+    { value: "2급", label: "2급" },
+    { value: "3급", label: "3급" },
+    { value: "4급", label: "4급" },
+    { value: "5급", label: "5급" },
     { value: "6급", label: "6급" },
-    { value: "strawberry", label: "Strawberry" },
-    { value: "vanilla", label: "Vanilla" },
 ];
 const wheelchair = [
     { value: "미사용", label: "미사용" },
@@ -37,7 +40,12 @@ const Receive = () => (
                         <Label for="tel">고객전화번호</Label>
                     </Col>
                     <Col>
-                        <NumberFormat format="###-####-####" id="tel" name="tel" className="form-control" />
+                        <div className="form-control-wrap">
+                            <NumberFormat format="###-####-####" id="tel" name="tel" className="form-control" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
                     </Col>
                     <Col xs="2" className="tit">
                         <Label for="purpose">이용목적</Label>
@@ -51,13 +59,23 @@ const Receive = () => (
                         <Label for="replyTel">회신번호</Label>
                     </Col>
                     <Col>
-                        <NumberFormat format="###-####-####" id="replyTel" name="replyTel" className="form-control" />
+                        <div className="form-control-wrap">
+                            <NumberFormat format="###-####-####" id="replyTel" name="replyTel" className="form-control" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
                     </Col>
                     <Col xs="2" className="tit">
                         <Label for="companion">동행인</Label>
                     </Col>
                     <Col>
-                        <Input type="number" name="companion" id="companion" />
+                        <div className="form-control-wrap">
+                            <Input type="number" name="companion" id="companion" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
@@ -65,13 +83,23 @@ const Receive = () => (
                         <Label for="name">고객명</Label>
                     </Col>
                     <Col>
-                        <Input type="text" name="name" id="name" />
+                        <div className="form-control-wrap">
+                            <Input type="text" name="name" id="name" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
                     </Col>
                     <Col xs="2" className="tit">
                         <Label for="receiveNum">고객접수번호</Label>
                     </Col>
                     <Col>
-                        <NumberFormat format="###-####-####" id="receiveNum" name="receiveNum" className="form-control" />
+                        <div className="form-control-wrap">
+                            <NumberFormat format="###-####-####" id="receiveNum" name="receiveNum" className="form-control" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
@@ -106,20 +134,20 @@ const Receive = () => (
                     <Col xs="2" className="tit">
                         접수서류
                     </Col>
-                    <Col className="d-flex justify-content-between">
-                        <FormGroup>
+                    <Col>
+                        <FormGroup check inline>
                             <Label check>
-                                <Input type="checkbox" id="receivePapers1" /> 심사신청서
+                                <Input type="checkbox" /> 심사신청서
                             </Label>
                         </FormGroup>
-                        <FormGroup>
-                            <Label check>
-                                <Input type="checkbox" id="receivePapers2" /> 이용동의서
+                        <FormGroup check inline>
+                            <Label>
+                                <Input type="checkbox" /> 이용동의서
                             </Label>
                         </FormGroup>
-                        <FormGroup>
-                            <Label check>
-                                <Input type="checkbox" id="receivePapers3" /> 복지카드 및 신분증사본
+                        <FormGroup check inline>
+                            <Label>
+                                <Input type="checkbox" /> 복지카드 및 신분증사본
                             </Label>
                         </FormGroup>
                     </Col>
@@ -128,17 +156,52 @@ const Receive = () => (
                     <Col xs="2" className="tit">
                         <Label for="vehicleIn">승차위치</Label>
                     </Col>
+                    <Col xs="6">
+                        <div className="form-control-wrap">
+                            <Input type="text" id="vehicleIn" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
+                    </Col>
                     <Col>
-                        <Input type="text" id="vehicleIn" />
+                        <div className="form-control-wrap">
+                            <Input type="text" id="vehicleIn" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
                     </Col>
                 </Row>
                 <Row>
                     <Col xs="2" className="tit">
                         <Label for="vehicleOut">하차위치</Label>
                     </Col>
-                    <Col>
-                        <Input type="text" id="vehicleOut" />
+                    <Col xs="6">
+                        <div className="form-control-wrap">
+                            <Input type="text" id="vehicleOut" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
                     </Col>
+                    <Col>
+                        <div className="form-control-wrap">
+                            <Input type="text" id="vehicleOut" />
+                            <span className="form-control-clear">
+                                <span className="blind">지우기</span>
+                            </span>
+                        </div>
+                    </Col>
+                    <Col xs="1" className="unit">
+                        분
+                    </Col>
+                </Row>
+                <Row>
+                    <Col xs="2" className="tit">
+                        <Label for="vehicleOut">예약</Label>
+                    </Col>
+                    <Col xs="6">{/* <Datetime dateFormat="YYYY-MM" timeFormat={false} /> */}</Col>
                 </Row>
             </Form>
         </div>
